f1c78841dbb014776c3ae9e3e5d68c330c45aa7e
Dyealex <dyealex@hughes.net>
2020-02-05 10:19:50 -0500

    added text

---
+++ codeAlong.js
@@ -0,0 +1,47 @@
+// Graded Code Along
+for (var i = 0; i < 1000; i++) {
+    if (i == 1 || i == 2) {
+        console.log(i);
+    } else if(i % 2 != 0){
+        // the number is not 1 or 2 and is odd
+        var J = 5;
+        while(j* <= i) {
+            if (i% == 0 || i%(J*2) == 0) 
+                return false
+                console.log(i);
+            }        }
+    
+
+
+
+
+
+var inventory = [
+    "Peanut Butter",(10) "Oreos", (2) "Yogurt", (4)  "Catfood", (3) "dog treats",(6) "treats for the cats", (5)]
+
+function add2Cart () {
+    var input = document.getElementById("input").nodeValue;
+    arr.forEach (array, function() {
+        console.log(array, [0]);
+        if (input==array [0]) {
+            cart.push(arrar [0]);
+            array [3]== 1
+        }
+    });
+
+inventory
+    for (let i=0; i < arr.length; i++) {
+        console.log(arr[i = 0])
+    }
+}
+// var groceryList = new Array();
+// groceryList[0]="Peanut Butter";
+// groceryList[1]="Oreos";
+// groceryList[2]="Yogurt";
+// groceryList[3]="Catfood";
+//groceryList[4]="dog treats";
+// groceryList[5]="treats for the cats";
+// groceryList[6]="milk";
+// groceryList[7]="bacon"
+// groceryList[8]="eggs";
+// groceryList[9]="bread";
