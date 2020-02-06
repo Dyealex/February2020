11117199880fc1dfbe00160a91079a96c32e44fe
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-02-06 18:33:58 -0500

    added text

---
+++ magicwordrev.js
@@ -0,0 +1,20 @@
+
+
+    document.getElementById("checkMagicWord").onclick =function(){
+			
+    var magicWordEntered=document.getElementById("magicWord").value;
+    
+    var magicWord = "Ironman";
+        
+    if (magicWordEntered==magicWord){
+            
+    alert("You are an Ironman!");
+    }
+        
+    else {
+            
+    alert ("Try again...");
+ }
+    }
+
+    
