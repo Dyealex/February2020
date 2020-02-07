d4ff0001b4e4acea45e2dfd5d8cd77463a5d87d2
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-02-07 01:25:32 -0500

    added text

--- magicwordrev.js
+++ magicwordrev.js
@@ -1,20 +1,22 @@
-
+// magicWord js file
 
     document.getElementById("checkMagicWord").onclick =function(){
 			
     var magicWordEntered=document.getElementById("magicWord").value;
-    
+   
     var magicWord = "Ironman";
-        
+    var magicWord2 = "ironman"   
+    
     if (magicWordEntered==magicWord){
             
     alert("You are an Ironman!");
-    }
-        
+    } else if (magicWordEntered==magicWord2) {
+       alert ("Try again, this is case-sensitive...")
+    }     
     else {
             
-    alert ("Try again...");
- }
+    alert ("Wrong! Try again...");
+    }
     }
 
     
