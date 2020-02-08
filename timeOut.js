7e928fd42fa0314c8397edc57c2e1943175c06a6
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-02-08 16:27:18 -0500

    added text

--- timeOut.js
+++ timeOut.js
@@ -1,13 +1,22 @@
 
 
-var timerStart
-
-    
-     function buttonStart (){
-        // document.getElementById('buttonStart').onclick=timerStart;
-         timerStart = window.setTimeout(window.alert, 2*1000, 'Man, you are slow!');
-          
+var timerStart 
+// let button1 = startCountdown
+// startCountdown= 1*2000;
+function timerStart(){
+    // timerStart.addEventListener("click", setTimeout, false);
+    // do while (se) {
+        // document.getElementById('textButton').innerHTML="Wait..."
+ timerStart.onclick(window.alert, 2*1000, 'Man, you are slow!');
+    //  function buttonStart ()
+}      
+    //   document.getElementById('textButton').innerHTML;"Wait..."; 
+        // 
+     
           function clearAlert ( ) {
               window.clearTimeout (timerStart);
+              
+              
           }
-        }
+        //   
+        // document.getElementById('textButton').innerHTML="Wait..."
